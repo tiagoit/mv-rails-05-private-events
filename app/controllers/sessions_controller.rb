b06b3ee4e3416485144ba9da451b126d4bbe8d93
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-19 08:20:35 -0800

    session validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new; end
-  def create; end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+
+    if @user
+      # TODO: loggin user
+    else
+      flash.now[:danger] = 'Invalid email'
+      render 'new'
+    end
+  end
+
   def destroy; end
 end

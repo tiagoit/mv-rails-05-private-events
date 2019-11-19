24237aab62ce83be4595b12ed2ac075be33eed4c
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-19 09:53:09 -0800

    working on users create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      log_in_helper(@user)
+      flash[:success] = 'Your account was created'
+      redirect_to user_path @user
+      # TODO: continue here
+    end
+  end
 
   def show; end
 

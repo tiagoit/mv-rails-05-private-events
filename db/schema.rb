fe6a956e168fd4c229fdaaab21bdb7d77e324f70
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-19 19:09:23 -0300

    add events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_155127) do
+ActiveRecord::Schema.define(version: 2019_11_19_220523) do
+
+  create_table "events", force: :cascade do |t|
+    t.text "description"
+    t.string "location"
+    t.datetime "date"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_11_19_155127) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "events", "users"
 end

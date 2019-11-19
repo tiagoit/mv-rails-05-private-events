1601527f8447d487004567125255059223d5020c
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-19 08:46:55 -0800

    logged in helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,10 +8,15 @@ module SessionsHelper
   def current_user_helper
     @current_user ||= User.find_by(id: session[:id]) if session[:user_id]
   end
-  
+
   # this method is used to destroy the actual user's session
   def log_out_helper
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # this method is used to validate if a user is logged
+  def logged_in_helper?
+    !current_user_helper.nil?
   end
 end

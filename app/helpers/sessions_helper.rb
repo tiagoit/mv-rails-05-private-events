b2b20372c34ec355fc9710b767bb67bebe2ff267
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-19 08:42:16 -0800

    log out helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,9 @@ module SessionsHelper
   def current_user_helper
     @current_user ||= User.find_by(id: session[:id]) if session[:user_id]
   end
+  
+  def log_out_helper
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end

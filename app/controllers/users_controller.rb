6dbeba50f3160fc24518012de705c345e278ca6c
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-19 09:47:06 -0800

    user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create; end
+
   def show; end
+
+  ################################################### private ##########################################################
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end

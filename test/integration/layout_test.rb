e596cad51b8f1eaf9beb06f50f40ebd879df4d41
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-20 14:05:29 -0800

    rubocop autofix 2

--- test/integration/layout_test.rb
+++ test/integration/layout_test.rb
@@ -5,19 +5,19 @@ class LayoutTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "layout links with non logged user" do
+  test 'layout links with non logged user' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 3
-    assert_select "a[href=?]", login_path, count: 1
+    assert_select 'a[href=?]', root_path, count: 3
+    assert_select 'a[href=?]', login_path, count: 1
   end
 
-  test "layout links with logged user" do
+  test 'layout links with logged user' do
     log_in_as(@user.email)
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", user_path(@user)
-    assert_select "a[href=?]", logout_path, count: 1
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', logout_path, count: 1
   end
 end

c533a1134afcaeb64aa3424f5e65816e9f465577
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-11-20 12:14:19 -0800

    index previous and next

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     return unless logged_in_helper?
-    
+
     @event = current_user_helper.events.build
     @events = Event.paginate(page: params[:page])
+    @previous = Event.joins(:attendees).previous_events
+    @next = Event.joins(:attendees).next_events
   end
 end

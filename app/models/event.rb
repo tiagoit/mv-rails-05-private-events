f872a6e9fcb7b71435cb195bade4902469a91463
Tiago Ferreira <tiferreira12@gmail.com>
2019-11-21 14:40:30 -0300

    has_many throuyght

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ApplicationRecord
   ################################################### relations ########################################################
   belongs_to :user
+
   has_many :attendees
+  has_many :users, through: :attendees
 
   #################################################### scopes ##########################################################
   scope :previous_events, -> { where('date < ?', Time.now) }
